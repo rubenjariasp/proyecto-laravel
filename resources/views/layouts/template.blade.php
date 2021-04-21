58c0092c69f322cfc7123cf1d0dd864b4b48bd54
ruben arias <ruprosperi2@gmail.com>
2021-04-21 19:17:23 -0400

    se creo el archivo css

---
+++ resources/views/layouts/template.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('css/estilos.css')}}">
+    <title>Proyecto Laravel</title>
+</head>
+<body>
+    <div class="container">
+        <header>
+            <nav class="nav-bar">
+                <a href="#" class="nav-bar__link">Deporte</a>
+                <a href="#" class="nav-bar__link">Drama</a>
+                <a href="#" class="nav-bar__link">Suspenso</a>
+            </nav>
+        </header>
+
+        <main>
+            <section class="section-main">
+                <h2 class="section-main__title">Titulo de la seccion</h2>
+            </section>
+
+            <aside class="sidebar">
+                <div class="widget"></div>
+                <div class="widget"></div>
+            </aside>
+        </main>
+
+        <footer>
+            <p>Pagina desarrollada por Rubén Arias. Todos los derechos reservados</p>
+        </footer>
+    </div>
+</body>
+</html>
